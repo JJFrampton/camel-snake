407455d01758948222b67de76f87db53b78bedf4
Joseph Frampton <josephjamesframpton@gmail.com>
2017-07-23 10:26:42 -0600

    M.1 renamed to 'camel-snake'
    -removed unused code
    
    JJF

---
+++ spec/camel-snake-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CamelSnakeView from '../lib/camel-snake-view';
+
+describe('CamelSnakeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
